c67c31200c8d7eedf7164ccd93079cc04db51d16
Tobias Oetiker <tobi@oetiker.ch>
2014-09-02 09:59:26 +0200

    remove notfound test

--- lib/Mojolicious/Command/generate/qx_mojo_app.pm
+++ lib/Mojolicious/Command/generate/qx_mojo_app.pm
@@ -889,15 +889,13 @@ use lib $FindBin::Bin.'/../backend/thirdparty/lib/perl5';
 use lib $FindBin::Bin.'/../backend/lib';
 
 
-use Test::More tests => 6;
+use Test::More tests => 4;
 use Test::Mojo;
 
 use_ok '<%= $p->{class} %>';
 use_ok '<%= $p->{controller} %>';
 
 my $t = Test::Mojo->new('<%= $p->{class} %>');
-
-$t->get_ok('/asdfasdf')->status_is(404);
 
 $t->post_ok('/jsonrpc','{"id":1,"service":"<%= $p->{name} %>","method":"ping","params":["hello"]}')
   ->json_is('',{id=>1,result=>'got hello'},'post request');
